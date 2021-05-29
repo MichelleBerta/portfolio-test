8e5558547c5810c540df90af99804bfd3faa5da4
Michelle Berta <bertafamily@sbcglobal.net>
2021-05-29 16:19:47 -0500

    added modals, set up workspace image to attach modals

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".carousel").carousel();
+  $(".materialboxed").materialbox();
+  $(".collapsible").collapsible();
+  $(".dropdown-trigger").dropdown();
+  $(".sidenav").sidenav();
+  $('.modal').modal();
+  });
+        
+
+  // Access toggle switch HTML element
+  var themeSwitcher = document.querySelector("#theme-switcher");
+  var container = document.querySelector(".container");
+
+  // Set default mode to dark
+  var darkMode = true;
+
+  // Listen for a click event on toggle switch
+  themeSwitcher.addEventListener("click", function () {
+    darkMode = !darkMode;
+    // If mode is dark, remove dark, apply bright background
+    if (!darkMode) {
+      container.classList.remove("dark");
+      container.classList.add("bright");
+    } else {
+      //remove bright, apply dark//
+      container.classList.remove("bright");
+      container.classList.add("dark");
+    }
+  });
+
