bbe2007c5a2ca0b6c3f7b3cb30d4b912a9967a28
Michelle Berta <bertafamily@sbcglobal.net>
2021-05-30 09:25:03 -0500

    changed some colors

--- script.js
+++ script.js
@@ -4,28 +4,26 @@ $(document).ready(function () {
   $(".collapsible").collapsible();
   $(".dropdown-trigger").dropdown();
   $(".sidenav").sidenav();
-  $('.modal').modal();
-  });
-        
+  $(".modal").modal();
+});
 
-  // Access toggle switch HTML element
-  var themeSwitcher = document.querySelector("#theme-switcher");
-  var container = document.querySelector(".container");
+// Access toggle switch HTML element
+var themeSwitcher = document.querySelector("#theme-switcher");
+var container = document.querySelector(".container");
 
-  // Set default mode to dark
-  var darkMode = true;
+// Set default mode to dark
+var darkMode = true;
 
-  // Listen for a click event on toggle switch
-  themeSwitcher.addEventListener("click", function () {
-    darkMode = !darkMode;
-    // If mode is dark, remove dark, apply bright background
-    if (!darkMode) {
-      container.classList.remove("dark");
-      container.classList.add("bright");
-    } else {
-      //remove bright, apply dark//
-      container.classList.remove("bright");
-      container.classList.add("dark");
-    }
-  });
-
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener("click", function () {
+  darkMode = !darkMode;
+  // If mode is dark, remove dark, apply bright background
+  if (!darkMode) {
+    container.classList.remove("dark");
+    container.classList.add("bright");
+  } else {
+    //remove bright, apply dark//
+    container.classList.remove("bright");
+    container.classList.add("dark");
+  }
+});
